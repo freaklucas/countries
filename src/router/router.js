d5cdd237102173c04a4c8f25b0b3e336f493aa70
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-25 22:40:44 -0300

    ultimas partes pra refatorar

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import Home from "../components/home/Home.vue";
-// import Search from "../components/template/Search.vue";
-import Region from "../components/template/Region.vue";
-import Capital from "../components/template/Capital.vue";
-import Lenguage from "../components/template/Lenguage.vue";
-import Country from "../components/template/Country.vue";
-import Code from "../components/template/Code.vue";
 
 Vue.use(Router);
 
@@ -17,31 +11,6 @@ export default new Router({
     {
       path: "/",
       component: Home,
-    },
-    {
-      path: "/region",
-      component: Region,
-      name: "region",
-    },
-    {
-      path: "/capital",
-      component: Capital,
-      name: "capital",
-    },
-    {
-      path: "/lenguage",
-      component: Lenguage,
-      name: "lenguage",
-    },
-    {
-      path: "/country",
-      component: Country,
-      name: "country",
-    },
-    {
-      path: "/code",
-      component: Code,
-      name: "code",
     },
   ],
 });
