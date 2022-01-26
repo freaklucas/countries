4a7af6914de69bf4ce0183421a1dd8d57ea181b7
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-25 22:37:09 -0300

    refatorando projeto

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="home">
     <Header />
-    <Menu />
     <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
 import Header from "../template/Header.vue";
-import Menu from "../template/Menu.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-    Menu,
   },
 };
 </script>
