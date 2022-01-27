2b4fb8f5225970f689e1e88ba1d6cf38b6c6b234
freaklucas <lucas.oliveira.freak@gmail.com>
2022-01-26 23:46:22 -0300

    add button

--- src/components/template/Search.vue
+++ src/components/template/Search.vue
@@ -12,7 +12,7 @@
           <option value="country">País</option>
           <option value="code">Código</option>
         </select>
-        <button class="but">Pesquisar:</button>
+        <button class="button-36">Pesquisar:</button>
         <div class="images">
           <img src="@/assets/images/bandeirabr.png" alt="bandeira do brasil" />
           <img
@@ -76,5 +76,44 @@ export default {};
 
 .selects {
   margin-bottom: 12px;
+}
+
+.button-36 {
+  background-image: linear-gradient(
+    92.88deg,
+    #455eb5 9.16%,
+    #5643cc 43.89%,
+    #673fd7 64.72%
+  );
+  border-radius: 8px;
+  border-style: none;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  flex-shrink: 0;
+  font-family: "Inter UI", "SF Pro Display", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  height: 2.6rem;
+  padding: 0 1.6rem;
+  text-align: center;
+  text-shadow: rgba(0, 0, 0, 0.25) 0 3px 8px;
+  transition: all 0.5s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-36:hover {
+  box-shadow: rgba(80, 63, 205, 0.5) 0 1px 30px;
+  transition-duration: 0.1s;
+}
+
+@media (min-width: 768px) {
+  .button-36 {
+    padding: 0 2rem;
+  }
 }
 </style>
